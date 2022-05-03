37da7d57c9ed477c83a2e6f90076b1a649c43933
kyllilum <kyllilumiste@gmail.com>
2022-05-03 15:02:48 +0300

    addNewUser

--- src/views/NewLoginView.vue
+++ src/views/NewLoginView.vue
@@ -1,25 +1,57 @@
 <template>
-  <div>
+  <div >
     <h3>Sisselogimiseks täida väljad</h3>
     <br>
 
     <div class="col-md-3 col-sm-12 mx-auto">
-      <div class="login-form">
+      <div v-if="tableDivDisplay" class="login-form">
         <form>
-          <div class="form-group">
+          <div  class="form-group">
             <input type="text" class="form-control" placeholder="Kasutajanimi">
           </div>
           <div class="form-group">
             <input type="password" class="form-control" placeholder="Parool">
           </div>
-          <button v-on:click="userLogin" type="submit" class="btn btn-success">Logi sisse</button>
+          <button v-on:click="$router.push('home')" type="submit" class="btn btn-success">Logi sisse</button>
           <br>
           <br>
+        </form>
           <label>Kasutaja puudub?</label>
           <br>
-          <button type="submit" class="btn btn-primary">Loo kasutaja</button>
-        </form>
+          <button type="button" class="btn btn-primary" v-on:click="hideTableDiv">Loo kasutaja</button>
+
+        </div>
+      <br>
+
+      <div>
+        <div v-if="newUserDisplay" class="login-form">
+          <form>
+            <div  class="form-group">
+              <input type="text" v-model= "contactFirstName"class="form-control" placeholder="Eesnimi">
+            </div>
+            <div class="form-group">
+              <input type="text" v-model="contactLastName" class="form-control" placeholder="Perekonnanimi">
+            </div>
+            <div class="form-group">
+              <input type="text" v-model="contactTelephone" class="form-control" placeholder="Telefon">
+            </div>
+            <div class="form-group">
+              <input type="text" v-model="contactEmail" class="form-control" placeholder="E-mail">
+            </div>
+            <div class="form-group">
+              <input type="text" v-model="username" class="form-control" placeholder="Kasutajanimi">
+            </div>
+            <div class="form-group">
+              <input type="password" v-model="password" class="form-control" placeholder="Parool">
+            </div>
+            <button v-on:click="$router.push('home'); addNewUser" type="submit" class="btn btn-success">Loo kasutaja</button>
+            <br>
+            <br>
+          </form>
+        </div>
       </div>
+
+
     </div>
 
   </div>
@@ -30,9 +62,15 @@ export default {
   name: "NewLoginView",
   data: function () {
     return {
+      tableDivDisplay: true,
+      newUserDisplay: false,
+      contactFirstName: '',
+      contactLastName: '',
+      contactTelephone: '',
+      contactEmail: '',
       username: '',
-      password: '',
-      successMessage: '',
+      password:''
+
     }
   },
   methods: {
@@ -42,13 +80,26 @@ export default {
       sessionStorage.setItem('user', JSON.stringify(this.user))
 
     },
-    userLogin: function () {
-      this.$http.get("/login", {
-            params: {
-              username: this.username,
-              password: this.password
-            }
-          }
+    hideTableDiv: function () {
+      this.tableDivDisplay = false;
+      this.newUserDisplay = true
+    },
+
+    displayTableDiv: function () {
+      this.tableDivDisplay = true;
+    },
+
+    addNewUser: function () {
+      let newUserInfo = {
+        contactFirstName: this.contactFirstName,
+        contactLastName: this.contactLastName,
+        contactTelephone: this.contactTelephone,
+        contactEmail: this.contactEmail,
+        username: this.username,
+        password: this.password
+      }
+
+      this.$http.post("/user/add", newUserInfo
       ).then(response => {
         console.log(response.data)
       }).catch(error => {
@@ -56,21 +107,9 @@ export default {
       })
     }
 
-    // userLogin: function () {
-    //   let userData = {
-    //     username: this.username,
-    //     password: this.password,
-    //   }
-    //
-    //   this.$http.post("/login", userData
-    //   ).then(response => {
-    //     this.successMessage = "Sisselogimine õnnestus, tere tuelmast."
-    //     console.log(response.data)
-    //   }).catch(error => {
-    //     console.log(error)
-    //   })
-    // }
   },
+
+
 }
 </script>
 
