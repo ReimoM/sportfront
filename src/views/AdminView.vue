134ddb35787308337d8d412122a91aa5936cd4a7
ReimoM <reimosalo@gmail.com>
2022-05-09 14:34:38 +0300

    Front Page update

--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,52 +1,47 @@
 <template>
-  <div >
-
-    <div  >
-      <div >
-      <button type="submit" v-on:click="showNewLocationTable" class="btn btn-outline-primary m-3">Lisa asukoht</button>
-      <button type="submit" class="btn btn-outline-primary m-3">Lisa väljak</button>
-      <button type="submit" class="btn btn-outline-primary m-3">Lisa spordiala</button>
-      <br>
-      <br>
-      <button type="submit" v-on:click="logOut" class="btn btn-outline-danger m-3">Logi välja</button>
+  <div>
+    <div class="col-md-3 col-sm-12 mx-auto">
+      <div v-if="adminButtons" class="login-form">
+        <button v-on:click="showNewLocationTable" type="submit" class="btn btn-outline-primary m-3" >Lisa asukoht</button>
+        <button type="submit" class="btn btn-outline-primary m-3">Lisa väljak</button>
+        <button type="submit" class="btn btn-outline-primary m-3">Lisa spordiala</button>
       </div>
-    </div>
-
-    <div>
-      <div v-if="newLocationTableDiv" class="login-form">
-        <div class="form-group">
-          <input type="text" v-model="locations.county" class="form-control" placeholder="Maakond">
+      <br>
+      <div>
+        <div v-if="newLocationTableDiv" class="login-form">
+          <div class="form-group">
+            <input type="text" v-model="locations.county" class="form-control" placeholder="Asukoht">
+          </div>
+          <button v-on:click="addNewLocation" type="submit" class="btn btn-success">Lisa asukoht</button>
+          <br>
+          <br>
         </div>
-
-        <button v-on:click="addNewLocation" type="submit" class="btn btn-success">Lisa asukoht</button>
-        <br>
-        <br>
       </div>
     </div>
   </div>
-
 </template>
 
 <script>
 export default {
   name: "AdminView",
-
-  data: {
-    locations: {},
-    newLocationTableDiv: false,
-    adminButtons: true
+  data: function () {
+    return {
+      locations: {},
+      newLocationTableDiv: false,
+      adminButtons: true
+    }
   },
 
   methods: {
     showNewLocationTable: function () {
       this.newLocationTableDiv = true
-      this.adminButtons = false
     },
 
     addNewLocation: function () {
       this.$http.post("/location", this.locations
       ).then(response => {
-        this.successMessage = 'Uus asukoht lisatud, maakond: ' + response.data.location + '.'
+        this.locations = response.data
+        this.successMessage = 'Uus asukoht lisatud, maakond: ' + response.data.locations + '.'
 
       }).catch(error => {
         console.log(error)
