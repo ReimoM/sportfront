4b90bfa4b6f03eff67affb618601764c995e513e
marjutamm <tamm.marju@gmail.com>
2022-05-09 14:23:19 +0300

    muutsin asju

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,14 +1,52 @@
 <template>
   <div>
+
+    <div>
     <h1>Siia aknasse tuled kui oled sisse loginud</h1>
     <button type="submit" class="btn btn-outline-primary m-3">Minu andmed</button>
-    <button type="submit" class="btn btn-outline-primary">Minu broneeringud</button>
+    <button type="submit" class="btn btn-outline-primary" v-on:click="hideTableDiv">Minu broneeringud</button>
     <br>
+
+      <div v-if="tableDivDisplay">
+        <table class="table table-hover table-bordered table-striped">
+
+          <thead>
+          <tr class="table-hover table-success">
+            <th scope="col">#</th>
+            <th scope="col">Väljak</th>
+            <th scope="col">Sport</th>
+            <th scope="col">Kuupäev</th>
+            <th scope="col">Broneeringu algus</th>
+            <th scope="col">Broneeringu lõpp</th>
+          </tr>
+          </thead>
+
+          <tbody>
+          <tr class="table-hover table-primary" v-for="booking in bookings">
+            <th scope="row">*</th>
+            <td>{{booking.sportsFieldName}}</td>
+            <td>{{booking.sportsType}}</td>
+            <td>{{booking.date}}</td>
+            <td>{{booking.startTimeHour}}</td>
+            <td>{{booking.endTimeHour}}</td>
+          </tr>
+          </tbody>
+
+        </table>
+
     <br>
     <button type="submit" v-on:click="logOut" class="btn btn-outline-danger m-3">Logi välja</button>
+    </div>
+
+
+
+    </div>
 
 
   </div>
+
+
+
 </template>
 
 <script>
@@ -18,6 +56,10 @@ export default {
     return {
       userId: sessionStorage.getItem('userId'),
       contacts: {},
+      bookings: [],
+      booking: {},
+      tableDivDisplay: false,
+      newUserDisplay: false
 
     }
 
@@ -39,9 +81,41 @@ export default {
       }).catch(error => {
         console.log(error)
       })
+    },
+
+    getMyBookings: function () {
+      this.$http.get("/field-booking/id", {
+        params: {
+          userId: this.userId
+        }
+      })
+          .then(response => {
+            this.bookings = response.data
+            console.log(response.data)
+          }).catch(error => {
+        console.log(error)
+      })
+    },
+
+    hideTableDiv: function () {
+      this.tableDivDisplay = true;
+    },
+
+    displayTableDiv: function () {
+      this.tableDivDisplay = true;
     }
+
+
+
+  },
+
+  mounted() {
+    this.getMyBookings()
   }
+
 }
+
+
 </script>
 
 <style scoped>
